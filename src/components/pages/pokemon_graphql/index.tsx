a6552c18f4b39b51906730204fcb6a03d38c8c37
lamechang-van <yoshimoto2107@gmail.com>
2023-03-17 17:55:05 +0900

    :lipstick:adjust style

--- src/components/pages/pokemon_graphql/index.tsx
+++ src/components/pages/pokemon_graphql/index.tsx
@@ -4,11 +4,16 @@ import PageContainer from "src/components/ui/PageContainer";
 import ContentSection from "src/components/ui/ContentSection";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_POKEMONS } from "src/queries/pokemon";
+import { Typography } from "src/components/ui/Typography";
+import { useGlobalValue } from "../../../context/hooks/index";
+import { muiThemeType } from "src/context/ui/theme";
+import clsx from "clsx";
 
 const PokemonPageComponent: NextPage = () => {
   const { loading, error, data } = useQuery(GET_ALL_POKEMONS, {
     variables: { first: 10 },
   });
+  const themeType = useGlobalValue(muiThemeType);
 
   return (
     <PageContainer>
@@ -16,22 +21,41 @@ const PokemonPageComponent: NextPage = () => {
 
       <ContentSection
         className={"p-4"}
-        title="BIO"
+        title="Pokemons"
         content={
-          <div>
-            {data &&
-              (
-                data.pokemons as Array<{
-                  id: string;
-                  name: string;
-                  image: string;
-                }>
-              ).map((pokemon) => (
-                <div key={pokemon.id}>
-                  <p>{pokemon.name}</p>
-                  <img src={pokemon.image} alt={pokemon.name} />
-                </div>
-              ))}
+          <div className="container mx-auto px-4 py-8">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {data &&
+                (
+                  data.pokemons as Array<{
+                    id: string;
+                    name: string;
+                    image: string;
+                  }>
+                ).map((pokemon) => (
+                  <div
+                    key={pokemon.id}
+                    className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center"
+                  >
+                    <img
+                      src={pokemon.image}
+                      alt={pokemon.name}
+                      className="w-32 h-32 mb-4"
+                    />
+                    <Typography
+                      className={clsx(
+                        "font-semibold",
+                        "text-sm",
+                        themeType === "light"
+                          ? "text-lightPrimaryMain"
+                          : "text-darkDefault"
+                      )}
+                    >
+                      {pokemon.name}
+                    </Typography>
+                  </div>
+                ))}
+            </div>
           </div>
         }
       />
