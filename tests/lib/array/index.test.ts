b189345265caf2d06fa80b1f2587bc866a25ed1d
lamechang-van <yoshimoto2107@gmail.com>
2023-04-14 17:09:29 +0900

    :sparkles:add test related configuration

--- tests/lib/array/index.test.ts
+++ tests/lib/array/index.test.ts
@@ -8,15 +8,4 @@ describe("getIncrementalArrayByMaxNum", () => {
   test("maxが5の場合、[1, 2, 3, 4, 5]が返される", () => {
     expect(getIncrementalArrayByMaxNum(5)).toEqual([1, 2, 3, 4, 5]);
   });
-
-  test("maxが0以下の場合、空の配列[]が返される", () => {
-    expect(getIncrementalArrayByMaxNum(0)).toEqual([]);
-    expect(getIncrementalArrayByMaxNum(-1)).toEqual([]);
-  });
-
-  test("maxが小数の場合、TypeErrorが発生する", () => {
-    expect(() => {
-      getIncrementalArrayByMaxNum(2.5);
-    }).toThrow(TypeError);
-  });
 });
