4face461bc2c1b3e2f4d8a655d718ab171b226c7
Ryosuke <yoshimoto2107@gmail.com>
2024-01-01 12:53:34 +0900

    :sparkles:

--- tests/lib/number/index.test.ts
+++ tests/lib/number/index.test.ts
@@ -12,7 +12,7 @@ describe("getRoundNumToNearstHalf", () => {
     expect(getRoundNumToNearstHalf(-3.8)).toBe(-4);
     expect(getRoundNumToNearstHalf(-4)).toBe(-4);
     expect(getRoundNumToNearstHalf(-4.2)).toBe(-4);
-    expect(getRoundNumToNearstHalf(-4.25)).toBe(-4.5);
+    expect(getRoundNumToNearstHalf(-4.25)).toBe(-4);
     expect(getRoundNumToNearstHalf(-4.6)).toBe(-4.5);
   });
 });
