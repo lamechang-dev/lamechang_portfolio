d99132caff9059b479a7f813da51eb61bb0d43b9
Ryosuke <yoshimoto2107@gmail.com>
2025-08-16 17:29:40 +0900

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from "next";
 import TopPageComponent from "src/components/pages";
+import { withCommonStaticProps } from "src/lib/withCommonStaticProps";
 
 const TopPage: NextPage = () => {
   return <TopPageComponent />;
 };
 
 export default TopPage;
+
+export const getStaticProps = withCommonStaticProps(async () => {
+  return { props: {} };
+});
