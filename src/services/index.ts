bb5e11ae4306f3ef78af08d321f1fc2ef390920a
Ryosuke Yoshimoto <lamechang@RyosukenoMacBook-Air.local>
2026-01-12 12:03:19 +0900

    :recycle:

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,12 @@
 import { Genre } from "src/domain/genres/model";
 import { Movie } from "src/domain/movies/model";
 
+const DEFAULT_REVALIDATE_TIME = 300;
+
 export const getGenres = async (): Promise<Array<Genre>> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/genres`,
-    { next: { revalidate: 300 } }
+    { next: { revalidate: DEFAULT_REVALIDATE_TIME } }
   );
   const data = await res.json();
   return data;
@@ -13,7 +15,7 @@ export const getGenres = async (): Promise<Array<Genre>> => {
 export const getMyFavoriteMovies = async (): Promise<Array<Movie>> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/movies/favorites`,
-    { next: { revalidate: 300 } }
+    { next: { revalidate: DEFAULT_REVALIDATE_TIME } }
   );
   const data = await res.json();
   return data;
@@ -22,7 +24,7 @@ export const getMyFavoriteMovies = async (): Promise<Array<Movie>> => {
 export const getMovieById = async (id: string): Promise<Movie> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/movies/${id}`,
-    { next: { revalidate: 300 } }
+    { next: { revalidate: DEFAULT_REVALIDATE_TIME } }
   );
   const data = await res.json();
   return data;
